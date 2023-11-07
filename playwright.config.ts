372703a0e540456253366c3b2129f2e93f4fc558
Emir <emir.sabanic.ro@gmail.com>
2023-11-07 21:38:11 +0100

    allure

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['allure-playwright', {outputDir: 'test-results'}]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     /* Maximum time one test can run for. */
     timeout: 30 * 6000,
@@ -36,7 +36,7 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
     headless: true,
     actionTimeout: 6000,
   },
