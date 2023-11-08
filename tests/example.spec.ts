5e6ce21fba2344ddd3c8f7d2d51cdaeffdb85743
Emir <emir.sabanic.ro@gmail.com>
2023-11-08 14:38:54 +0100

    changes to env var

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,6 +2,6 @@ import { test, expect } from '@playwright/test';
 import ENV from '../utils/env';
 
 test('has title', async ({ page }) => {
-  await page.goto(ENV.API_URL);
+  await page.goto(ENV.API_URL!);
   await page.pause();
 });
